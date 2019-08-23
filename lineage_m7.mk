39b9996d2dad2e9d9ccad294f8af667f8d43daf2
TARKZiM <tom8476oo@gmail.com>
2019-08-23 23:26:24 +0800

    m7: Fix build fingerprint

--- lineage_m7.mk
+++ lineage_m7.mk
@@ -2,11 +2,9 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 $(call inherit-product, device/htc/m7/full_m7.mk)
 
-# Device naming
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := m7
 PRODUCT_NAME := lineage_m7
 
-# Override build props
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT="htc/m7_google/m7:5.1/LMY47O.H18/666675:user/release-keys" \
-    BUILD_ID=LMY47O.H18 \
-    PRIVATE_BUILD_DESC="6.04.1700.18 CL536258 release-keys"
+# Device Fingerprint
+BUILD_FINGERPRINT := htc/m7_google/m7:5.1/LMY47O.H18/666675:user/release-keys
